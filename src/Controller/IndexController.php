0c77a448229b072ddb7a579d36bcf8705227ccd8
ra0ued <ra0ued@zabtech.ru>
2018-11-03 20:01:28 +0900

    Logging and error handling basic implementation.

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -3,44 +3,72 @@
 namespace App\Controller;
 
 use App\Service\TrackService;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
     /**
+     * @var Session
+     */
+    private $session;
+
+    public function __construct()
+    {
+        $this->session = new Session();
+        $this->session->start();
+    }
+
+    /**
      * @Route("/", name="index")
      */
     public function indexAction()
     {
-        return $this->render('fixer/index.html.twig', []);
+        $errors = $this->session->getFlashBag()->get('error', []);
+
+        $this->session->clear();
+
+        return $this->render('fixer/index.html.twig', [
+            'errors' => $errors,
+        ]);
     }
 
     /**
      * @Route("/upload", name="upload_file")
      * @param Request $request
      * @param TrackService $trackService
-     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
+     * @param LoggerInterface $logger
+     * @return \Symfony\Component\HttpFoundation\BinaryFileResponse|\Symfony\Component\HttpFoundation\RedirectResponse
      */
-    public function uploadAction(Request $request, TrackService $trackService)
+    public function uploadAction(Request $request, TrackService $trackService, LoggerInterface $logger)
     {
         /** @var \Symfony\Component\HttpFoundation\File\UploadedFile $file */
         $file = $request->files->get('track', false);
 
-        if(!$file) {
-            throw new BadRequestHttpException('Upload gpx file.');
+        if (!$file) {
+            $logger->error('Wrong file format.');
+            $this->session->getFlashBag()->add('error', 'Нужно загрузитъ GPX файл.');
+
+            return $this->redirectToRoute('index');
         }
 
         if ($file->getClientOriginalExtension() !== 'gpx' && $file->getMimeType() !== 'text/xml') {
-            throw new BadRequestHttpException('Wrong file type. Only gpx allowed.');
+            $logger->error('Wrong file content ' . $file->getClientOriginalName());
+            $this->session->getFlashBag()->add('error', 'Содержимое файла не похоже на GPX :(');
+
+            return $this->redirectToRoute('index');
         }
 
         $fixedTrack = $trackService->handleFile($file);
 
-        if(!$fixedTrack) {
-            throw new BadRequestHttpException('Something wrong.');
+        if (!$fixedTrack) {
+            $logger->error('Something wrong with ' . $file->getClientOriginalName());
+            $this->session->getFlashBag()->add('error', 'Что-то пошло не так, напишите на ra0ued@zabtech.ru');
+
+            return $this->redirectToRoute('index');
         }
 
         return $this->file($fixedTrack, 'Fixed_' . $file->getClientOriginalName());
